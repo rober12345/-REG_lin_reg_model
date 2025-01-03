33108fc05f11b2d6dee3eb48606f030edf7aee74
Roberto Ugalde <94400091+rober12345@users.noreply.github.com>
2025-01-03 11:04:22 -0600

    Update app.py
    add 
    print("Database connection established successfully!")

--- src/app.py
+++ src/app.py
@@ -6,10 +6,11 @@ import sys
 import os
 
 # Add the src directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+sys.path.append(os.path.join(os.path.-REG_lin_reg_model(__file__), 'src'))
 
 # Now import db_connect
 from utils import db_connect
 
 # Connect to the database
 engine = db_connect()
+print("Database connection established successfully!")
