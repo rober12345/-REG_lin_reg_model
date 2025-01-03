eda5c05a780e14574c16e9e5b5b58797996eb417
Roberto Ugalde <94400091+rober12345@users.noreply.github.com>
2025-01-03 11:07:59 -0600

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,6 @@ load_dotenv()
 
 def db_connect():
     import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
+    engine = create_engine('postgresql://postgres:ManzanaOrganico1@localhost/reg_lin_reg')
     engine.connect()
     return engine
