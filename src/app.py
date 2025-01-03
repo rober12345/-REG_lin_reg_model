ac9c5b26a8670ebd3964ff7248abda7aa04c600e
Roberto Ugalde <94400091+rober12345@users.noreply.github.com>
2025-01-03 10:56:18 -0600

    Update app.py
    Add the Path to sys.path

--- src/app.py
+++ src/app.py
@@ -2,3 +2,14 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+# Now import db_connect
+from utils import db_connect
+
+# Connect to the database
+engine = db_connect()
